344c5535ef2e74141ee0725e3b3d64a0d3e3b9a6
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-14 15:02:55 -0500

    commit tree 38339990790a2114a8f046dae599fd6723efee6b

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string) {
+    const wordArray = string.split(' ');
+    const ignoredWords = [
+      'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'
+    ]
+
+    return wordArray.map((word, index) => {
+      if(!ignoredWords.includes(word) || index == 0) {
+        return this.capitalize(word);
+      }
+      return word;
+    }).join(' ');
+  }
 }
+
+const string = "the road to el dorado"
+
+console.log(Formatter.titleize(string))
